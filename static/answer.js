9b0120ea218a9adfedccdc2eec5dab49481bf3c0
Ant2000code <59964873+Ant2000code@users.noreply.github.com>
2020-10-17 20:12:29 +0530

    Basic UI+reg/login/logout+ posting ques/ans

---
+++ static/answer.js
@@ -0,0 +1,27 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  function myFunction(){
+      
+      alert("Question has been Submitted");
+  }
+  var doc = new jsPDF(); 
+  var specialElementHandlers = { 
+  '#editor': function (element, renderer) { 
+  return true; 
+  } 
+  };
+  $("#submit").click(function () { 
+  doc.fromHTML($("#content").html(), 15, 15, { 
+  'width': 190, 
+  'elementHandlers': specialElementHandlers 
+  }); 
+  doc.save('sample-page.pdf'); 
+  });
